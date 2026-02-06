d0a1512fdd7d7c80a3e60456f751efe31b819738
muhsin <69848808+muhsinyildirimm@users.noreply.github.com>
2026-02-06 15:15:05 +0300

    Update date.h
    exception throw feature added

--- date.h
+++ date.h
@@ -1,9 +1,9 @@
-#pragma once
 #ifndef DATE_H
 #define DATE_H
 
 #include <iosfwd>
 #include <ctime>
+#include "date_exception.h"
 
 namespace project {
     class Date {
@@ -11,30 +11,30 @@ namespace project {
         static constexpr int year_base = 1900;
         static constexpr int random_min_year = 1940;
         static constexpr int random_max_year = 2020;
-
-        enum class Weekday {
-            Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+        
+        enum class Weekday { 
+            Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday 
         };
-
+        
         // Constructors
         Date();
         Date(int d, int m, int y);
         explicit Date(const char* p);
         explicit Date(std::time_t timer);
-
+        
         // Getters
         [[nodiscard]] int get_month_day() const;
         [[nodiscard]] int get_month() const;
         [[nodiscard]] int get_year() const;
         [[nodiscard]] int get_year_day() const;
         [[nodiscard]] Weekday get_week_day() const;
-
+        
         // Setters
         Date& set_month_day(int day);
         Date& set_month(int month);
         Date& set_year(int year);
         Date& set(int day, int mon, int year);
-
+        
         // Arithmetic operators
         [[nodiscard]] Date operator-(int day) const;
         Date& operator+=(int day);
@@ -43,36 +43,36 @@ namespace project {
         Date operator++(int);
         Date& operator--();
         Date operator--(int);
-
+        
         // Static functions
         [[nodiscard]] static Date random_date();
         static constexpr bool isleap(int y);
-
+        
         // Friend declarations for comparison operators
         friend bool operator<(const Date&, const Date&);
         friend bool operator==(const Date&, const Date&);
-
+        
         // Friend declarations for stream operators
         friend std::ostream& operator<<(std::ostream& os, const Date& date);
         friend std::istream& operator>>(std::istream& is, Date& date);
-
+        
     private:
         int m_day;
         int m_month;
         int m_year;
     };
-
+    
     // Global comparison operators
     [[nodiscard]] bool operator<=(const Date&, const Date&);
     [[nodiscard]] bool operator>(const Date&, const Date&);
     [[nodiscard]] bool operator>=(const Date&, const Date&);
     [[nodiscard]] bool operator!=(const Date&, const Date&);
-
+    
     // Date arithmetic
     [[nodiscard]] int operator-(const Date& d1, const Date& d2);
     [[nodiscard]] Date operator+(const Date& date, int n);
     [[nodiscard]] Date operator+(int n, const Date&);
-
+    
     // Weekday operators
     Date::Weekday& operator++(Date::Weekday& r);
     Date::Weekday operator++(Date::Weekday& r, int);
